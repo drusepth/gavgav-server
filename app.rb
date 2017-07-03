bc6de78a9b9540c396aa20120dfaa829b15b5752
Andrew Brown <drusepth@gmail.com>
2017-07-03 02:45:32 +0000

    Basic routing flow for router > controller > view

--- app.rb
+++ app.rb
@@ -1,8 +1,37 @@
 require 'rack'
+require 'pry'
+require 'erb'
+
+# require_relative stuff
+
+class Router
+  def self.route(path)
+    # /my/test/cool/foo should instantiate CoolController in controllers/my/test/cool_controller.rb, which has method foo on it
+    _, *controller_path, receiving_method = path.split('/')
+
+    controller = instantiate_receiving_controller(controller_path)
+    controller_response_binding = controller.new.send(receiving_method)
+
+    view_template_source = File.read("views/#{controller_path.join('/')}/#{receiving_method}.html.erb")
+    erb_renderer = ERB.new(view_template_source)
+
+    erb_renderer.result(controller_response_binding)
+  end
+
+  private
+
+  def self.instantiate_receiving_controller controller_path
+    require_relative "controllers/#{controller_path.join('/')}_controller.rb"
+    Object.const_get("#{controller_path.last.capitalize}Controller")
+  end
+end
 
 app = Proc.new do |env|
-
-    ['200', {'Content-Type' => 'text/html'}, [env.inspect]]
+    [
+      '200',
+      { 'Content-Type' => 'text/html' },
+      [ Router.route(env['PATH_INFO']) ]
+    ]
 end
 
 Rack::Handler::WEBrick.run app
---
+++ controllers/test_controller.rb
@@ -0,0 +1,9 @@
+class TestController
+  def some_public_method
+  end
+
+  private
+
+  def some_private_method
+  end
+end
